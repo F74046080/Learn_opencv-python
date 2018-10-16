e5ea4a72c9496610c569856283c24e9c655687a6
F74046080 <for1426@gmail.com>
2018-10-17 01:50:02 +0800

    Add rotation

--- transform.py
+++ transform.py
@@ -4,7 +4,7 @@ import numpy as np
 a = cv2.imread('images/OriginalTransform.png')
 cv2.imshow('origin', a)
 print(a.shape)
-# rows -> height   columns -> width
+# rows -> height  columns -> width
 rows, cols, channel = a.shape
 
 # translation position
@@ -13,6 +13,10 @@ ty = 50
 translation = np.float32([[1, 0, tx], [0, 1, ty]])
 tra_dst = cv2.warpAffine(a, translation, (cols, rows))
 cv2.imshow('img',tra_dst)
+
+rotate = cv2.getRotationMatrix2D((130+tx, 125+ty), 45, 2)
+rot_dst = cv2.warpAffine(tra_dst, rotate, (cols, rows))
+cv2.imshow('rotate', rot_dst)
 
 
 cv2.waitKey(0)
