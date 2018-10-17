ad832164fe76c236988a116c5afa83c2125aa7f4
F74046080 <for1426@gmail.com>
2018-10-18 01:33:46 +0800

    Add magnitude threshold and trackbar to set threshold value

--- edge_detection.py
+++ edge_detection.py
@@ -17,9 +17,39 @@ def sobel(P):
     return (np.abs((P[0] + 2 * P[1] + P[2]) - (P[6] + 2 * P[7] + P[8])) +
             np.abs((P[2] + 2 * P[6] + P[7]) - (P[0] + 2 * P[3] + P[6])))
 screw_sobely = generic_filter(screw_smooth, sobel_y, (3, 3))
-cv2.imshow('sobel_y', screw_sobely)
+# cv2.imshow('sobel_y', screw_sobely)
 screw_sobelx = generic_filter(screw_smooth, sobel_x, (3, 3))
-cv2.imshow('sobel_x', screw_sobelx)
+# cv2.imshow('sobel_x', screw_sobelx)
 screw_sobel = generic_filter(screw_smooth, sobel, (3, 3))
 cv2.imshow('sobel', screw_sobel)
+
+ret,screw_threshold = cv2.threshold(screw_sobel, 100, 255, cv2.THRESH_BINARY)
+# cv2.imshow('threshold', screw_threshold)
+np.set_printoptions(threshold=np.nan)
+# print(np.where(screw_threshold < 0))
+def magnitude_threshold(x):
+    magnitude_value = cv2.getTrackbarPos('bar','magnitude')    
+    ret,screw_threshold = cv2.threshold(screw_sobel, magnitude_value, 255, cv2.THRESH_BINARY)
+    cv2.imshow('magnitude', screw_threshold)
+
+cv2.namedWindow('magnitude')
+cv2.createTrackbar('bar', 'magnitude', 40, 255, magnitude_threshold)
+ret,screw_threshold = cv2.threshold(screw_sobel, 40, 255, cv2.THRESH_BINARY)
+cv2.imshow('magnitude', screw_threshold)
+
+# theta_mat = np.degrees(np.arctan2(screw_sobely, screw_sobelx))
+theta_mat = np.arctan2(screw_sobely, screw_sobelx)*180/np.pi
+# print(screw_sobelx.shape)
+# print(theta_mat.shape)
+# print(np.where(theta_mat > 100))
+# print(theta_mat)
+
+
+# def direction_threshold(x):
+#     theta = cv2.getTrackbarPos('theta', 'orientation')
+# cv2.namedWindow('orientation')
+# cv2.createTrackbar('theta', 'orientation', 40, 255, direction_threshold)
+
+
+
 cv2.waitKey(0)
